4cba8bb867339dbe27177aedf743186ea57f367d
qyao <qyao4@academic.rrc.ca>
2024-03-09 23:20:32 -0600

    populate drivers_routes table from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,22 +19,43 @@
 # end
 
 # Populate the 'routes' table with 10 entries.(API)
-require 'net/http'
-require 'json'
-require 'uri'
+# require 'net/http'
+# require 'json'
+# require 'uri'
 
-route_keys = ["BLUE", "642", "650", "691", "694", "47", "74","16","18","31"]
+# route_keys = ["BLUE", "642", "650", "691", "694", "47", "74","16","18","31"]
 
-route_keys.each do |route_key|
-  url = URI("https://api.winnipegtransit.com/v3/routes/#{route_key}.json?api-key=aaitNO7SFcfMFhmbrg-u")
-  response = Net::HTTP.get(url)
-  result = JSON.parse(response)
+# route_keys.each do |route_key|
+#   url = URI("https://api.winnipegtransit.com/v3/routes/#{route_key}.json?api-key=aaitNO7SFcfMFhmbrg-u")
+#   response = Net::HTTP.get(url)
+#   result = JSON.parse(response)
 
-  route_data = result['route']
+#   route_data = result['route']
 
-  Route.create!(
-    number: route_data['number'],
-    customer_type: route_data['customer-type'],
-    coverage: route_data['coverage']
-  )
+#   Route.create!(
+#     number: route_data['number'],
+#     customer_type: route_data['customer-type'],
+#     coverage: route_data['coverage']
+#   )
+# end
+
+# Populate the 'drivers_routes' table with csv
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'drivers_routes.csv'))
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  driver_id = row['driver_id']
+  route_id = row['route_id']
+
+  driver = Driver.find_by(id: driver_id)
+  route = Route.find_by(id: route_id)
+
+  if driver && route
+    driver.routes << route unless driver.routes.include?(route)
+  else
+    puts "Driver or Route not found: Driver ID = #{driver_id}, Route ID = #{route_id}"
+  end
 end
+
+puts "Imported drivers_routes associations from CSV"
